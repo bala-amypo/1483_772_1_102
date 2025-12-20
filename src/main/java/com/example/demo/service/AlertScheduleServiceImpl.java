26878dee2f77d8ada1f18ff4acdd6fb2712d6844
bala-amypo <bala@amypo.in>
2025-12-20 14:51:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AlertScheduleServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.AlertSchedule;
+import com.example.demo.entity.Warranty;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.AlertScheduleRepository;
+import com.example.demo.repository.WarrantyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AlertScheduleServiceImpl
+        implements AlertScheduleService {
+
+    @Autowired
+    private AlertScheduleRepository alertScheduleRepository;
+
+    @Autowired
+    private WarrantyRepository warrantyRepository;
+
+    @Override
+    public AlertSchedule createSchedule(Long warrantyId, AlertSchedule schedule) {
+
+        if (schedule.getDaysBeforeExpiry() == null ||
+                schedule.getDaysBeforeExpiry() < 0) {
+            throw new IllegalArgumentException("daysBeforeExpiry");
+        }
+
+        Warranty warranty = warrantyRepository.findById(warrantyId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Warranty not found"));
+
+        schedule.setWarranty(warranty);
+
+        return alertScheduleRepository.save(schedule);
+    }
+
+    @Override
+    public List<AlertSchedule> getSchedules(Long warrantyId) {
+        return alertScheduleRepository.findByWarrantyId(warrantyId);
+    }
+}
