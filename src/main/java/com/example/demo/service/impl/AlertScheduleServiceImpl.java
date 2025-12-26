29b7daf1b60eb15b82be35359ed0a50cd67327e0
bala-amypo <bala@amypo.in>
2025-12-26 06:49:39 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertScheduleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertScheduleServiceImpl.java
@@ -1,21 +1,57 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.AlertSchedule;
+import com.example.demo.entity.Warranty;
+import com.example.demo.repository.AlertScheduleRepository;
+import com.example.demo.repository.WarrantyRepository;
 import com.example.demo.service.AlertScheduleService;
 import org.springframework.stereotype.Service;
-import java.util.Collections;
+
 import java.util.List;
 
 @Service
 public class AlertScheduleServiceImpl implements AlertScheduleService {
 
+    private final AlertScheduleRepository scheduleRepository;
+    private final WarrantyRepository warrantyRepository;
+
+    public AlertScheduleServiceImpl(
+            AlertScheduleRepository scheduleRepository,
+            WarrantyRepository warrantyRepository
+    ) {
+        this.scheduleRepository = scheduleRepository;
+        this.warrantyRepository = warrantyRepository;
+    }
+
     @Override
     public AlertSchedule createSchedule(Long warrantyId, AlertSchedule schedule) {
-        return schedule;
+
+        Warranty warranty = warrantyRepository.findById(warrantyId)
+                .orElseThrow(() ->
+                        new RuntimeException("Warranty not found")
+                );
+
+        if (schedule.getDaysBeforeExpiry() == null ||
+            schedule.getDaysBeforeExpiry() < 0) {
+
+            throw new IllegalArgumentException(
+                    "daysBeforeExpiry must be >= 0"
+            );
+        }
+
+        schedule.setWarranty(warranty);
+
+        return scheduleRepository.save(schedule);
     }
 
     @Override
     public List<AlertSchedule> getSchedules(Long warrantyId) {
-        return Collections.emptyList();
+
+        warrantyRepository.findById(warrantyId)
+                .orElseThrow(() ->
+                        new RuntimeException("Warranty not found")
+                );
+
+        return scheduleRepository.findByWarrantyId(warrantyId);
     }
 }
